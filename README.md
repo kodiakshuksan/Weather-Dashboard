9fa940f401e1bf2f76a50320cb333482b135cacc
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-02-02 00:10:19 -0800

    was able to get fetch and get a response

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather-Dashboard
+by Jennifer Detmering
+
+## Description
+This is basic weather dashboard that provides a current look at weather conditions and a five-day future forecast. 
+
+## Deployment
+
+
+## Project Image
+
+
+## Technologies 
+For this project I used Visual Studio Code, HTML, CSS, and Javascript.
+
+
+## Challenges
+1/29/2021- research apis cdn
+1/30/2021- added HTML and Javascript file, tried fetched with no luck
+2/1/2021- was able to make a fetch call and get a response started
+thinking about layout and design
+
+## Contributors 
--- script.js
+++ script.js
@@ -1,28 +1,170 @@
 //Javascript
 console.log("hello")
 
-function getApi() {
+
   
-  var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=335fc46330e5a6ab7ec6dbd91233522c';
-console.log(requestUrl)
-  fetch(requestUrl)
+//getting Current weather 
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Tacoma&appid=335fc46330e5a6ab7ec6dbd91233522c'
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        cityList.appendChild(listItem);
-      }
+      console.log(data);
     });
-}
 
-searchBtn.addEventListener('click', getApi);
+//getting Future Forecast
+    fetch(
+      'https://api.openweathermap.org/data/2.5/forecast?q=Tacoma&appid=335fc46330e5a6ab7ec6dbd91233522c'
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+
+
+      //getting uv index
+      fetch(
+        'https://api.openweathermap.org/data/2.5/uvi?lat=47.2529&lon=-122.4443&appid=335fc46330e5a6ab7ec6dbd91233522c'
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+  
+  //defining units in Fahrenheit
+      fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=47.2529&lon=-122.4443&units=imperial&appid=335fc46330e5a6ab7ec6dbd91233522c'
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+
+// defining time
+
+      fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=47.2529&lon=-122.443&exclude=hourly,daily&appid=335fc46330e5a6ab7ec6dbd91233522c'
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+  
 
 
 
 
 
-//api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key335fc46330e5a6ab7ec6dbd91233522c}
+
+// fetch(
+//   // Explain each parameter in comments below.
+//   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc'
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+// // Parameter explanation.
+// The per_page parameter will limit the results base per page.
+// The state parameter r
+
+
+
+
+  
+
+      // var searchFormEl = document.querySelector('#search-form');
+
+      // function handleSearchFormSubmit(event) {
+      //   event.preventDefault();
+      
+      //   var searchInputVal = document.querySelector('#search-input').value;
+      //   var formatInputVal = document.querySelector('#format-input').value;
+      
+      //   if (!searchInputVal) {
+      //     console.error('You need a search input value!');
+      //     return;
+      //   }
+      
+      //   var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+      
+      //   location.assign(queryString);
+      // }
+      
+      // searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+      
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch(
+//   // Explain each parameter in comments below.
+//   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc'
+// )
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+// // Parameter explanation.
+// The per_page parameter will limit the results base per page.
+// The state parameter r
+
+
+ // getting upto date future forecast connect to files
+      // fetch(
+      //   'http://bulk.openweathermap.org/snapshot/{BULK_FILE_NAME}?appid=335fc46330e5a6ab7ec6dbd91233522c'
+      // )
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     console.log(data);
+      //   });
+
+      // getting all cities connect to files
+      // fetch(
+      //   'http://bulk.openweathermap.org/snapshot/{BULK_FILE_NAME}?appid=335fc46330e5a6ab7ec6dbd91233522c'
+      // )
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     console.log(data);
+      //   });
+
+        
+  // getting upto date current forecast connect to files
+      // fetch(
+      //   'http://bulk.openweathermap.org/snapshot/{BULK_FILE_NAME}?appid=335fc46330e5a6ab7ec6dbd91233522c'
+      // )
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     console.log(data);
+      //   });
